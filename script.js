5f47663fa2d5c214802184569e6b272307e98c5e
Joshua <joshuanaulak2010@gmail.com>
2026-07-23 12:36:36 +0530

    Converted options array to contain object too

--- script.js
+++ script.js
@@ -13,25 +13,90 @@ const questions = [
     {
         key:"travel",
         question:"How do you travel daily?",
-        options: ["Car","Bus", "Train", "Walk"],
+        //options: ["Car","Bus", "Train", "Walk"],
+        options: [
+            {
+                text: "Car",
+                need_input: true,
+            },
+
+            {
+                text: "Bus",
+                need_input: true,
+            },
+            {
+                text: "Train",
+                need_input: true,
+            },
+            {
+                text: "Walk",
+                need_input: true,
+            }
+        ],
         input_Text: "How many Km per day (approx)?"
     },
 
     {
         key:"food",
         question:"How often do you eat meat?",
-        options: ["Almost everyday", "Few times per month", "Few times per year", "I dont eat meat"],
+        //options: ["Almost everyday", "Few times per month", "Few times per year", "I dont eat meat"],
+        options: [
+            {
+                text: "Almost everyday",
+                need_input: true,
+            },
+
+            {
+                text: "Few times per month",
+                need_input: true,
+            },
+            {
+                text: "Few times per year",
+                need_input: true,
+            },
+            {
+                text: "I dont eat meat",
+                need_input: false,
+            }
+        ],
         input_Text: "How many times per month?"
     },
     {
         key:"recycle",
         question:"Do you recycle?",
-        options: ["Yes", "No"]
+        //options: ["Yes", "No"]
+        options:[
+            {
+                text: "Yes",
+                need_input:false,
+            },
+
+            {
+                text: "No",
+                need_input: false,
+            }
+        ]
     },
     {
         key: "test",
         question:"Testing if 4 options work",
-        options: ["YIPPIE", " LETS GO", "BROSKIE"]
+        //options: ["YIPPIE", " LETS GO", "BROSKIE"]
+        options:[
+            {
+                text: "YIPPIE",
+                need_input:false,
+            },
+
+            {
+                text: "LETS GO",
+                need_input: false,
+            },
+
+            {
+                text: "Broskie",
+                need_input: false,
+            }
+        ]
     }
     
 ]
@@ -42,7 +107,6 @@ function saveAns(){
     checkbox.forEach(box => {
         let Number_Value = box.parentElement.parentElement.querySelector(".Number-value").value
         if(box.checked){
-            
             answers[questions[question_No].key] = {}
             answers[questions[question_No].key].options = box.value
             answers[questions[question_No].key].value = Number_Value //eg. answers.travel.value => answers{
@@ -103,6 +167,7 @@ function isCheck(){
 }
 
 function next_question(ques_no){
+    console.log(Options.length)
     for(let i = 0; i < Options.length; i++){
         let checkboxLabel = Options[i].querySelector("span");
         let checkbox2 = Options[i].querySelector(".check");
@@ -114,11 +179,10 @@ function next_question(ques_no){
             //checkboxLabel.style.display = "";
             
             inputLabel.textContent = questions[ques_no].input_Text
-            checkboxLabel.textContent = questions[ques_no].options[i];
+            checkboxLabel.textContent = questions[ques_no].options[i].text;
             checkbox2.value = questions[ques_no].options[i];
             checkbox2.checked = false;
             number_input.value = "0";
-            
             Value_select.style.display = "";
         }
         else{
